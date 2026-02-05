7c4fcdfa333fb89374c274fe35475232736d1894
MadheshSiva <madheshsiva28@gmail.com>
2026-02-05 16:02:10 +0530

    lastest changes

--- index.js
+++ index.js
@@ -1,101 +1,101 @@
-// const express = require('express');
-// const cors = require('cors');
-// const mongodbConnection = require('./src/connectionDB/mongodbConnection');
-// const {createUser, getAllusers, updateUser, deleteuser, userIsDelete, userInactive} = require('./src/controller/users/users')
-// const {createProduct,getAllProducts,getProductById,getUserProductDetails} = require('./src/controller/products/product')
-// require('dotenv').config();
-
-// const app = express();
-// const PORT = 3060 || process.env.PORT;
-// app.use(cors({
-//     origin: true,
-//     credentials: true
-// }));
-// app.use(express.json());
-// const Router = express.Router();
-
-// // User Routes
-// Router.post('/users-create',createUser);
-// Router.get('/users-getall',getAllusers);
-// Router.put('/users-update/:id',updateUser);
-// Router.delete('/users-delete/:id',deleteuser);
-// Router.patch('/users-inactive/:id',userInactive);
-// Router.patch('/users-isdelete/:id',userIsDelete);
-// Router.post('/products-create',createProduct);
-// Router.get('/products-getall',getAllProducts);
-// Router.get('/products-getbyid/:id',getProductById);
-// Router.get('/products-userdetails',getUserProductDetails);
-// app.use('/api',Router);
-// mongodbConnection().then(() => {
-// app.listen(PORT, () => {
-//     console.log(`Server connected on Port: ${PORT}`);
-// })
-// }).catch((err) => {
-//     console.log('Failed to connect to the database', err);
-// })
-
 const express = require('express');
 const cors = require('cors');
-const serverless = require('serverless-http');
 const mongodbConnection = require('./src/connectionDB/mongodbConnection');
-
-const {
-  createUser,
-  getAllusers,
-  updateUser,
-  deleteuser,
-  userIsDelete,
-  userInactive
-} = require('../src/controller/users/users');
-
-const {
-  createProduct,
-  getAllProducts,
-  getProductById,
-  getUserProductDetails
-} = require('../src/controller/products/product');
-
+const {createUser, getAllusers, updateUser, deleteuser, userIsDelete, userInactive} = require('./src/controller/users/users')
+const {createProduct,getAllProducts,getProductById,getUserProductDetails} = require('./src/controller/products/product')
 require('dotenv').config();
 
 const app = express();
-
+const PORT = 3060 || process.env.PORT;
 app.use(cors({
-  origin: true,
-  credentials: true
+    origin: true,
+    credentials: true
 }));
-
 app.use(express.json());
-
 const Router = express.Router();
 
 // User Routes
-Router.post('/users-create', createUser);
-Router.get('/users-getall', getAllusers);
-Router.put('/users-update/:id', updateUser);
-Router.delete('/users-delete/:id', deleteuser);
-Router.patch('/users-inactive/:id', userInactive);
-Router.patch('/users-isdelete/:id', userIsDelete);
+Router.post('/users-create',createUser);
+Router.get('/users-getall',getAllusers);
+Router.put('/users-update/:id',updateUser);
+Router.delete('/users-delete/:id',deleteuser);
+Router.patch('/users-inactive/:id',userInactive);
+Router.patch('/users-isdelete/:id',userIsDelete);
+Router.post('/products-create',createProduct);
+Router.get('/products-getall',getAllProducts);
+Router.get('/products-getbyid/:id',getProductById);
+Router.get('/products-userdetails',getUserProductDetails);
+app.use('/api',Router);
+mongodbConnection().then(() => {
+app.listen(PORT, () => {
+    console.log(`Server connected on Port: ${PORT}`);
+})
+}).catch((err) => {
+    console.log('Failed to connect to the database', err);
+})
 
-// Product Routes
-Router.post('/products-create', createProduct);
-Router.get('/products-getall', getAllProducts);
-Router.get('/products-getbyid/:id', getProductById);
-Router.get('/products-userdetails', getUserProductDetails);
+// const express = require('express');
+// const cors = require('cors');
+// const serverless = require('serverless-http');
+// const mongodbConnection = require('./src/connectionDB/mongodbConnection');
 
-app.use('/api', Router);
+// const {
+//   createUser,
+//   getAllusers,
+//   updateUser,
+//   deleteuser,
+//   userIsDelete,
+//   userInactive
+// } = require('../src/controller/users/users');
 
-// Connect DB once
-let isConnected = false;
-const connectDB = async () => {
-  if (!isConnected) {
-    await mongodbConnection();
-    isConnected = true;
-  }
-};
+// const {
+//   createProduct,
+//   getAllProducts,
+//   getProductById,
+//   getUserProductDetails
+// } = require('../src/controller/products/product');
 
-app.use(async (req, res, next) => {
-  await connectDB();
-  next();
-});
+// require('dotenv').config();
 
-module.exports = serverless(app);
+// const app = express();
+
+// app.use(cors({
+//   origin: true,
+//   credentials: true
+// }));
+
+// app.use(express.json());
+
+// const Router = express.Router();
+
+// // User Routes
+// Router.post('/users-create', createUser);
+// Router.get('/users-getall', getAllusers);
+// Router.put('/users-update/:id', updateUser);
+// Router.delete('/users-delete/:id', deleteuser);
+// Router.patch('/users-inactive/:id', userInactive);
+// Router.patch('/users-isdelete/:id', userIsDelete);
+
+// // Product Routes
+// Router.post('/products-create', createProduct);
+// Router.get('/products-getall', getAllProducts);
+// Router.get('/products-getbyid/:id', getProductById);
+// Router.get('/products-userdetails', getUserProductDetails);
+
+// app.use('/api', Router);
+
+// // Connect DB once
+// let isConnected = false;
+// const connectDB = async () => {
+//   if (!isConnected) {
+//     await mongodbConnection();
+//     isConnected = true;
+//   }
+// };
+
+// app.use(async (req, res, next) => {
+//   await connectDB();
+//   next();
+// });
+
+// module.exports = serverless(app);
